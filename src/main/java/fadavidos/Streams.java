88fbb34ec8e39e63b022a2e6c09897c3432f9b0e
Fabian Osorio <fadavidos@gmail.com>
2023-05-12 17:20:59 -0500

    stream unit test

---
+++ src/main/java/fadavidos/Streams.java
@@ -0,0 +1,14 @@
+package fadavidos;
+
+import java.io.IOException;
+import java.util.stream.IntStream;
+
+public class Streams {
+
+    public static void main(String[] args) throws IOException {
+
+        IntStream.range(1, 10)
+                .forEach(System.out::print);
+
+    }
+}
